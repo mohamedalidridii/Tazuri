210caec92e225fc4a0dcb65f6b3b05dd3ee1c680
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2022-04-13 05:05:43 +0200

    NavBar

--- client/public/index.js
+++ client/public/index.js
@@ -1,3 +1,44 @@
+
+/**********************/
+/*Text animation */
+/**********************/
+const aleoVera = document.querySelector(".aleo-vera");
+const aleoVeraImg= document.querySelector(".aleo-vera-img");
+const plante1= document.querySelector(".plante_1");
+const plante1Img= document.querySelector(".plante_1_img");
+
+/*Hover Effect */
+window.addEventListener("mousemove", (e)=>{
+    let x=e.offsetX;
+    let y= e.offsetY;
+
+if  (e.target.classList.contains("aleo-vera")) {
+    aleoVeraImg.style.left= `${x}px`;
+    aleoVeraImg.style.top= `${y}px`;
+        
+}
+if  (e.target.classList.contains("plante_1")) {
+    plante1Img.style.left= `${x}px`;
+    plante1Img.style.top= `${y}px`;
+        
+}
+});
+/*PLante1*/
+plante1.addEventListener("mouseover", () => {
+    plante1Img.style.display="block";
+});
+plante1.addEventListener("mouseleave", () => {
+    plante1Img.style.display="";
+});
+/*Aleo-Vera*/
+aleoVera.addEventListener("mouseover", () => {
+    aleoVeraImg.style.display="block";
+});
+aleoVera.addEventListener("mouseleave", () => {
+    aleoVeraImg.style.display="";
+});
+
+/********Menu*****************/
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
